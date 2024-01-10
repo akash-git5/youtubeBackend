ce09f07e7f50d75910186dd1d4aad4df65e194bc
akash git5 <akash.coding2021@gmail.com>
2024-01-11 01:36:45 +0530

    few changes is done regarding [ require("dotenv").config() ]

--- src/index.js
+++ src/index.js
@@ -9,3 +9,7 @@ dbConnect();
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on ${process.env.PORT}`);
 })
+
+// app.get("/", (req,res) => {
+//     res.send(`<h3>Believe in yourself And NEVER GIVE UP !</h3>`);
+// })
