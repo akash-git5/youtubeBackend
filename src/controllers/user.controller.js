c94bae69b5053382c5482297e8d58182a48361fa
akash git5 <akash.coding2021@gmail.com>
2024-01-12 21:13:01 +0530

    all setup configured

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+
+
+const registerUser = async (req,res) => {
+    res.status(200).json({
+        message: "hey there !"
+    })
+}
+
+module.exports = registerUser
--- src/index.js
+++ src/index.js
@@ -13,13 +13,26 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.use(cookieParser());
+
+
+
+// importing routes :
+const userRoutes = require("./routes/user.routes")
+
+
+
+// defining routers :
+app.use("/api/v1/user",userRoutes);
+
+
 
 
 dbConnect();
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on PORT: ${process.env.PORT}`);
+    console.log(`⚙️ Server is running on PORT: ${process.env.PORT}`);
 })
 
 // app.get("/", (req,res) => {
